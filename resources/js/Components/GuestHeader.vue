4ba47b6c59d9b5a0d02198b80864ffc8565451e4
Alexis Brian Mango <alexi@frba.utn.edu.ar>
2025-05-30 22:58:28 -0300

    Added animated transition to Welcome view.

--- resources/js/Components/GuestHeader.vue
+++ resources/js/Components/GuestHeader.vue
@@ -1,52 +1,111 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import { reactive, ref } from 'vue';
+import { Link } from '@inertiajs/vue3'
+import { reactive, ref, onMounted } from 'vue'
+
+function smoothScrollTo(targetEl, duration = 800) {
+    const target = document.querySelector(targetEl)
+    if (!target) return
+
+    const headerOffset = 90
+    const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - headerOffset
+    const startPosition = window.pageYOffset
+    const distance = targetPosition - startPosition
+    let startTime = null
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime
+        const timeElapsed = currentTime - startTime
+        const run = easeInOutQuad(timeElapsed, startPosition, distance, duration)
+        window.scrollTo(0, run)
+        if (timeElapsed < duration) requestAnimationFrame(animation)
+    }
+
+    function easeInOutQuad(t, b, c, d) {
+        t /= d / 2
+        if (t < 1) return c / 2 * t * t + b
+        t--
+        return -c / 2 * (t * (t - 2) - 1) + b
+    }
+
+    requestAnimationFrame(animation)
+}
+
+const handleAnchorClick = (e, href) => {
+    e.preventDefault()
+    const anchor = href.split('#')[1]
+    if (anchor) smoothScrollTo(`#${anchor}`)
+    isMenuVisible.value = false
+}
 
 const navItems = reactive([
-    { type: 'normal', name : 'Inicio', href : '/' },
-    { type: 'normal', name : 'Nosotros', href : '/' },
-    { type: 'normal', name : 'Especialidades', href : '/' },
-    { type: 'normal', name : 'Contacto', href : '/' },
-    { type: 'normal', name : 'Administracion', href : route('login'), class : 'bg-indigo-500 text-white p-2 ms-1 rounded-sm hover:text-white hover:border-none w-auto' },
-    { 
-      type : 'icon', name : 'fas fa-cart-shopping', 
-      href : route('cart.checkout'),
-      class: 'text-indigo-500',
-      text: 'Mi carrito'
+    { type: 'top', name: 'Inicio', href: '#' },
+    { type: 'anchor', name: 'Nosotros', href: '/#nosotros' },
+    { type: 'anchor', name: 'Especialidades', href: '/#especialidades' },
+    { type: 'anchor', name: 'Contacto', href: '/#contactanos' },
+    {
+        type: 'normal',
+        name: 'Ingresar',
+        href: route('login'),
+        class: 'bg-indigo-500 text-white p-2 ms-1 rounded-sm hover:text-white hover:border-none w-auto'
+    },
+    {
+        type: 'icon',
+        name: 'fas fa-cart-shopping',
+        href: route('cart.checkout'),
+        class: 'text-indigo-500',
+        text: 'Mi carrito'
     }
 ])
 
 const isMenuVisible = ref(false)
-const toggleMenu = () => { 
+const toggleMenu = () => {
     isMenuVisible.value = !isMenuVisible.value
 }
 </script>
+
 <template>
     <header class="w-full bg-white fixed shadow-sm top-0 start-0 z-30">
         <div class="w-11/12 px-3 flex mx-auto justify-between items-center">
             <Link :href="route('welcome')">
-                <img src="/images/logo.jpg" class="w-[85px]" />
+            <img src="/images/logo.jpg" class="w-[85px]" />
             </Link>
             <nav class="navigation-items hidden md:block">
-                <Link v-for="item of navItems" :href="item.href" class="mx-3" :class="item.class ?? ''">
+                <template v-for="item of navItems">
+                    <a v-if="item.type === 'anchor' || item.type === 'top'" :href="item.href"
+                        class="mx-3 hover:underline cursor-pointer" :class="item.class ?? ''" @click.prevent="() => {
+                            isMenuVisible = false;
+                            if (item.type === 'top') {
+                                smoothScrollTo('body');
+                            } else {
+                                const hash = item.href.split('#')[1];
+                                smoothScrollTo(`#${hash}`);
+                            }
+                        }">
+                        {{ item.name }}
+                    </a>
+
+                    <Link v-else :href="item.href" class="mx-3" :class="item.class ?? ''"
+                        @click="isMenuVisible = false">
                     <template v-if="item.type == 'normal'">
                         <span>{{ item.name }}</span>
                     </template>
                     <template v-else>
-                        <i :class="item.name"></i>
+                        <i :class="item.name"></i> {{ item.text }}
                     </template>
-                </Link>
+                    </Link>
+                </template>
             </nav>
             <i class="fa-solid fa-bars fa-xl cursor-pointer md:hidden" @click="toggleMenu"></i>
         </div>
         <nav class="navigation-items block md:hidden border-t">
-            <Link v-for="item of navItems" :href="item.href" class="mx-3 block py-2 mb-1" :class="item.class ?? ''" v-if="isMenuVisible">
-                <template v-if="item.type == 'normal'">
-                    <span>{{ item.name }}</span>
-                </template>
-                <template v-else>
-                    <i :class="item.name"></i> {{ item.text }}
-                </template>
+            <Link v-for="item of navItems" :href="item.href" class="mx-3 block py-2 mb-1" :class="item.class ?? ''"
+                v-if="isMenuVisible">
+            <template v-if="item.type == 'normal'">
+                <span>{{ item.name }}</span>
+            </template>
+            <template v-else>
+                <i :class="item.name"></i> {{ item.text }}
+            </template>
             </Link>
         </nav>
     </header>
