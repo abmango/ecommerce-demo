4611b47269022614d04256c6a03f6030479bf1c2
Daniel Martinez <dmyh96@gmail.com>
2025-11-03 18:43:58 -0300

    optimizando script de deploy

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-php ../composer.phar install
-php ../composer.phar dump-autoload --optimize
-php artisan optimize:clear
-php artisan migrate 
+php_82 = "/opt/cloudlinux/alt-php82/root/usr/bin/php"
+$php_82 ../composer.phar install
+$php_82 ../composer.phar dump-autoload --optimize
+$php_82 artisan optimize:clear
+$php_82 artisan migrate 
 wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
